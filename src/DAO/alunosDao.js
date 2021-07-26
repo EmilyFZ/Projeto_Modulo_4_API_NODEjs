6aa290ca507d3217dd7b89d8f30b939f283355db
EmilyFZ <61170696+EmilyFZ@users.noreply.github.com>
2021-07-26 15:25:36 -0300

    Atualizando insereAluno

--- src/DAO/alunosDao.js
+++ src/DAO/alunosDao.js
@@ -7,7 +7,25 @@ class AlunosDAO {
 
     visualizaTodosAluno = () => {};
 
-    insereAluno = () => {};
+    insereAluno = (aluno) =>{
+        return new Promise ((resolve, reject)=>{
+            this.db.run(
+                `INSERT INTO ALUNOS (NOME, TURMA, EMAIL) VALUES(?,?,?)`,
+                alunos.nome,
+                alunos.turma,
+                alunos.email,
+                (err)=>{
+                    if(err){
+                        reject(err)
+                    }else{
+                        resolve(aluno)
+                    }
+                }
+            )
+        })
+
+    };
+        
 
     atualizaAluno = () => {};
 
