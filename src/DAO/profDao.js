f99c2882b49efa90072c22dbf29ca628b633cf00
EmilyFZ <61170696+EmilyFZ@users.noreply.github.com>
2021-07-26 15:26:24 -0300

    Atualizando Prof /Dao

--- src/DAO/profDao.js
+++ src/DAO/profDao.js
@@ -11,8 +11,24 @@ class ProfessorDAO {
 
     };
 
-    insereProf = () => {
+    insereProf = (professor) =>{
+        return new Promise ((resolve, reject)=>{
+            this.db.run(
+                `INSERT INTO PROFESSORES (NOME, DISCIPLINA, EMAIL) VALUES(?,?,?)`,
+                professor.nome,
+                professor.disciplina,
+                professor.email,
+                (err)=>{
+                    if(err){
+                        reject(err)
+                    }else{
+                        resolve(professor)
+                    }
+                }
+            )
+        })
 
+    };
     };
 
     atualizaProf = () => {
